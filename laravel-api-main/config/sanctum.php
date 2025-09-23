cd47c16071e05f5f535727a3410d4abef088305b
WilliansNavas <wiliansn20002@gmail.com>
2025-09-22 22:15:43 -0600

    fix(sanctum) se soluciona error de verificacion de token

--- laravel-api-main/config/sanctum.php
+++ laravel-api-main/config/sanctum.php
@@ -16,8 +16,9 @@ return [
     */
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
-        '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+        '%s%s%s',
+        'localhost,localhost:3000,localhost:5173,127.0.0.1,127.0.0.1:8000,127.0.0.1:5173,::1',
+        ',empresa1.localhost,empresa1.localhost:5173,empresa2.localhost,empresa2.localhost:5173',
         Sanctum::currentApplicationUrlWithPort()
     ))),
 
