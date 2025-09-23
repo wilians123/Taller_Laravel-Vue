12033c2267a89d1c35d1ae8c290c686db6e5dda0
WilliansNavas <wiliansn20002@gmail.com>
2025-09-22 23:34:53 -0600

    feat(cors) manejo de subdominios

--- laravel-api-main/config/cors.php
+++ laravel-api-main/config/cors.php
@@ -19,9 +19,16 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'http://127.0.0.1:5173',
+        'http://empresa1.localhost:5173',
+        'http://empresa2.localhost:5173',
+    ],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [
+        '/^http:\/\/.*\.localhost:5173$/',
+    ],
 
     'allowed_headers' => ['*'],
 
@@ -29,6 +36,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
